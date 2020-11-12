f56365b96b7d7fee9318875c417b456b71978f82
Gregorio Martocci <gregoriomartocci@gmail.com>
2020-11-12 05:08:53 -0300

    Mostramos el total del carrito de compras

--- amazonclone/src/Components/Checkout.js
+++ amazonclone/src/Components/Checkout.js
@@ -7,7 +7,7 @@ function Checkout() {
     <div className="checkout">
       <div className="checkout__left">
         <img className="checkout__ad" src="/banners/banner__2.jpg"></img>
-        
+
         <div>
           <h2 className="checkout__title"> Your shopping Basket</h2>
         </div>
